0dc25f58d576a2f8e74eb3f77ec97a569a965018
IronPegasus <kayleebanana@gmail.com>
2021-07-24 15:05:53 -0500

    21w29b, updated to .NET Core 3.1

--- LambdaEntryPoint.cs
+++ LambdaEntryPoint.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using System.IO;
 
 namespace RedditEmblemAPI
 {
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using RedditEmblemAPI.Services;
 
 namespace RedditEmblemAPI
@@ -38,7 +33,7 @@ namespace RedditEmblemAPI
                 });
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRazorPages().AddNewtonsoftJson();
 
             // Add S3 to the ASP.NET Core dependency injection framework.
             services.AddAWSService<Amazon.S3.IAmazonS3>();
@@ -46,7 +41,7 @@ namespace RedditEmblemAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -59,7 +54,11 @@ namespace RedditEmblemAPI
 
             app.UseCors(AllowedOriginsPolicy);
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
         }
     }
 }
