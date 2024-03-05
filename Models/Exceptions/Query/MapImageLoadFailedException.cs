fa315ad7817e63b70f7bde7563e4c6c6a41c05ec
Iron Pegasus <kayleebanana@gmail.com>
2024-03-04 22:45:40 -0600

    24w10a, adding custom error message for 404 response to map image load

---
+++ Models/Exceptions/Query/MapImageLoadFailedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RedditEmblemAPI.Models.Exceptions.Query
+{
+    internal class MapImageLoadFailedException : Exception
+    {
+        /// <summary>
+        /// Constructor for when the image load failure is the result of a 404 not found error.
+        /// </summary>
+        public MapImageLoadFailedException() 
+            : base($"The map image load failed with a \"404 Not Found\" message. Does the map image URL point to a valid and accessible image file?")
+        { }
+
+        /// <summary>
+        /// Constructor for when the image load failure is NOT the result of a 404 not found error.
+        /// </summary>
+        public MapImageLoadFailedException(string exceptionMessage) 
+            : base($"The map image load failed with the following message: {exceptionMessage}")
+        { }
+    }
+}
