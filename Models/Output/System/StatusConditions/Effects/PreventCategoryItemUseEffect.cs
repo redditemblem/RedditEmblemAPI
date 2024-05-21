9d9b7665307ec6648c0fd33f5ff245a7714391a7
Iron Pegasus <kayleebanana@gmail.com>
2024-05-20 20:00:53 -0500

    24w21a, PreventCategoryItemUse status condition effect

---
+++ Models/Output/System/StatusConditions/Effects/PreventCategoryItemUseEffect.cs
@@ -0,0 +1,39 @@
+﻿using RedditEmblemAPI.Models.Exceptions.Validation;
+using RedditEmblemAPI.Models.Output.Units;
+using RedditEmblemAPI.Services.Helpers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RedditEmblemAPI.Models.Output.System.StatusConditions.Effects
+{
+    public class PreventCategoryItemUseEffect : StatusConditionEffect
+    {
+        #region Attributes
+
+        protected override string Name { get { return "PreventCategoryItemUse"; } }
+        protected override int ParameterCount { get { return 1; } }
+
+        /// <summary>
+        /// Param1. List of item categories to check for.
+        /// </summary>
+        private List<string> Categories { get; set; }
+
+        #endregion
+
+        public PreventCategoryItemUseEffect(List<string> parameters)
+            : base(parameters)
+        {
+            this.Categories = DataParser.List_StringCSV(parameters, INDEX_PARAM_1);
+
+            if (!this.Categories.Any())
+                throw new RequiredValueNotProvidedException(NAME_PARAM_1);
+        }
+
+        public override void Apply(Unit unit, StatusCondition status, IDictionary<string, Tag> tags)
+        {
+            //Mark use as prevented for all items with a category configured in Categories
+            foreach (UnitInventoryItem item in unit.Inventory.Items.Where(i => this.Categories.Contains(i.Item.Category)))
+                item.IsUsePrevented = true;
+        }
+    }
+}
